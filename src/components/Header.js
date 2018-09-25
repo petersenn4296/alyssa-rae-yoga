5d65f914752dae2af844b36a85eaf085d9eb6ee2
Peter S <petersenn4296@gmail.com>
2018-09-25 13:55:21 -0600

    no more warnings, skeleton ready for alyssas input

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Link, Route } from 'react-router-dom'
-import About from './About'
+import { Link } from 'react-router-dom'
 import '../App.css';
 
 class Header extends Component {
@@ -9,7 +8,7 @@ class Header extends Component {
       <div className='navbar-fixed'>
         <nav>
           <div className="nav-wrapper">
-            <Link to="/" className="brand-logo"><img height='60' width='150' src={require('../pics/first_copy.png')}/></Link>
+            <Link to="/" className="brand-logo"><img height='60' width='150' alt='alyssa rae yoga logo' src={require('../pics/first_copy.png')}/></Link>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
               <li><Link to="/about">About</Link></li>
               <li><Link to="/flows">Flows</Link></li>
@@ -17,7 +16,6 @@ class Header extends Component {
             </ul>
           </div>
         </nav>
-        <Route path="/about" component={About}/>
       </div>
 
     );
